3336e8b23d3b4fd99ca0bbc1e90cc4713683a4a4
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-27 15:15:48 +0100

    fix project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import dev.kordex.gradle.plugins.kordex.DataCollection
+
 plugins {
     val kotlinVersion = "2.1.0"
 
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    // id("dev.kordex.gradle.kordex") version "1.5.8"
+    id("dev.kordex.gradle.kordex") version "1.5.8"
 }
 
 group = "live.einfachgustaf"
@@ -12,21 +14,6 @@ description = "A Discord bot for the EinfachGustaf community"
 
 repositories {
     mavenCentral()
-
-    maven {
-        name = "KordEx (Releases)"
-        url = uri("https://repo.kordex.dev/releases")
-    }
-
-    maven {
-        name = "KordEx (Snapshots)"
-        url = uri("https://repo.kordex.dev/snapshots")
-    }
-
-    maven {
-        name = "Sonatype Snapshots (Legacy)"
-        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-    }
 }
 
 dependencies {
@@ -37,19 +24,17 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.0")
     implementation("dev.kord:kord-core:0.15.0")
-    implementation("dev.kordex:kord-extensions:2.3.1-SNAPSHOT") // TODO: remove when plugin is working
 }
 
 kotlin {
     jvmToolchain(21)
 }
 
-/*
+
 kordEx {
     bot {
-        dataCollection(DataCollection.Standard)
+        dataCollection(DataCollection.None)
 
         mainClass = "live.einfachgustaf.sharkbot.EntrypointKt"
     }
 }
- */

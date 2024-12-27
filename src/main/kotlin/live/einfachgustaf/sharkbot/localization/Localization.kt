1562c4e720e06b689b03ecdb6c2189566aa864b3
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-27 15:44:15 +0100

    add localization

---
+++ src/main/kotlin/live/einfachgustaf/sharkbot/localization/Localization.kt
@@ -0,0 +1,57 @@
+package live.einfachgustaf.sharkbot.localization
+
+import java.util.*
+
+/**
+ * The localization class.
+ *
+ * @param language The language of the localization.
+ */
+class Localization(language: String) {
+
+    /**
+     * The bundle of the localization.
+     */
+    val bundle = ResourceBundle.getBundle("lang/$language")
+
+    /**
+     * Get a localized string.
+     *
+     * @param key The key of the string.
+     * @param replacements The replacements for the string.
+     * @return The localized string.
+     */
+    fun get(
+        key: String,
+        replacements: Map<String, String> = mapOf()
+    ): String {
+        return replacements.entries
+            .fold(bundle.getString(key)) { acc, (key, value) ->
+                acc.replace("{$key}", value)
+            }
+    }
+
+    companion object {
+        /**
+         * List of all available languages.
+         */
+        val allLanguages: List<String> by lazy {
+            val resourceBundle = ResourceBundle.getBundle("lang/messages", Locale.ROOT)
+            resourceBundle.keySet().map { it.split("_").last() }.distinct()
+        }
+
+        /**
+         * Calculate the percentage of translations completed for each language.
+         */
+        fun calculateTranslationPercentages(): Map<String, Double> {
+            val enUSBundle = ResourceBundle.getBundle("lang/en_US")
+            val enUSKeys = enUSBundle.keySet().size
+
+            return allLanguages.associateWith { language ->
+                val bundle = ResourceBundle.getBundle("lang/$language")
+                val keys = bundle.keySet().size
+                (keys.toDouble() / enUSKeys) * 100
+            }
+        }
+    }
+}

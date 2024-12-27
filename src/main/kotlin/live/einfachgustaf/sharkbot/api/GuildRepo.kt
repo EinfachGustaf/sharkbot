22e6d151bc907ad12378a6e92c21dbc332d0f3b1
Marlon <72864666+DinoMarlir@users.noreply.github.com>
2024-12-27 15:21:28 +0100

    add javadocs for api

--- src/main/kotlin/live/einfachgustaf/sharkbot/api/GuildRepo.kt
+++ src/main/kotlin/live/einfachgustaf/sharkbot/api/GuildRepo.kt
@@ -2,9 +2,34 @@ package live.einfachgustaf.sharkbot.api
 
 import dev.kord.common.entity.Snowflake
 
+/**
+ * The guild repository.
+ * Used for handling guilds.
+ */
 class GuildRepo {
 
+    /**
+     * Get a guild by its snowflake.
+     *
+     * @param id The snowflake of the guild.
+     * @return The guild or null if it doesn't exist.
+     */
     fun getGuildBySnowflake(id: Snowflake): SharkGuild? = TODO("Not yet implemented")
+
+    /**
+     * Setup a guild by its snowflake.
+     * If the guild already exists, it will return the existing guild.
+     *
+     * @param id The snowflake of the guild.
+     * @return The guild.
+     */
     fun setupGuild(id: Snowflake): SharkGuild = TODO("Not yet implemented")
+
+    /**
+     * Get a guild by its snowflake or set up a new guild.
+     *
+     * @param id The snowflake of the guild.
+     * @return The guild.
+     */
     fun getGuildOrSetup(id: Snowflake): SharkGuild = TODO("Not yet implemented")
 }
--- src/main/kotlin/live/einfachgustaf/sharkbot/api/SharkUser.kt
+++ src/main/kotlin/live/einfachgustaf/sharkbot/api/SharkUser.kt
@@ -3,6 +3,12 @@ package live.einfachgustaf.sharkbot.api
 import dev.kord.core.entity.User
 import live.einfachgustaf.sharkbot.models.UserModel
 
+/**
+ * The SharkUser class.
+ *
+ * @param user The user.
+ * @param info The user info.
+ */
 class SharkUser(
     val user: User,
     val info: UserModel
